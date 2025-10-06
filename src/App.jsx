b55e5ce0a2b2842cc077a3150ce791c248c35946
VT-Owais <vtowais786@gmail.com>
2025-10-06 23:46:33 +0530

    updated portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar.jsx';
+import Navbar from './Components/Navbar/Navbar.jsx';
 import Home from './pages/home/Home';
 import Projects from './pages/projects/Projects';
 import './App.css';
